d995dc3ee756d1611c9473ac94047e7fd899316c
Roman Štofa <247183@vutbr.cz>
2024-04-18 15:40:59 +0200

    Princip prihlasenia

--- BPC_Bank/bankProfile.cs
+++ BPC_Bank/bankProfile.cs
@@ -3,73 +3,69 @@ using System.Text;
 using System.Security.Cryptography;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
 
 class bankProfile{
-
-// Security info
-    private string profilePrivateSID; // Privatne SecurityID, ktore vygenerujeme na zaklade ID
-    private string profilePublicSID; // Public SecurityID, ktore vygenerujeme na zaklade ID
 
 // Osobne info
     private string profileEmail;
     private string profileAddress;
     private int profileID;
     private string profileName;
-    private string profilePassword;
 
 
     public bankProfile(){
         
     }
+    //Tu sa loadne profile do objektu, ktory nasledne budeme pouzivat ako prihlaseneho uzivatela.
+    public int loadProfile(string profileName, string profilePassword){
+        string databaseName; //sem pride pripojenie do databazy
+        return 1;
+    }
+
+    public int unloadProfile(){
+        this.profileEmail = null;
+        this.profileName = null;
+        this.profileAddress = null;
+        this.profileID = 0;
+        return 1;
+    }
 
 // Funkcia, ktora vezme udaje uzivatela a vytvori mu ucet.
-    public int createProfile(string profileName, string profilePassword, string profileEmail, string profileAddress){ // Vytvorenie profilu s menom a ID (napr obciansky, ...)
+    public int createProfile(string profileEmail, string profileAddress){ // Vytvorenie profilu s menom a ID (napr obciansky, ...)
         
         return 1;
     }
 
 //Funkcia, ktora na zaklade ID profilu a hesla vymaze ucet.
-    public int deleteProfile(string profileID, string profilePassword){
+    public int deleteProfile(){
         // Vymazanie uctu na zaklade ID profilu z databaze.
         return 1;
     }
 
-    public int changeProfileInfo(string profileID, string profilePassword, string profileEmail, string profileAddress){
-        if(profileEmail == null){
-            profileEmail = profileEmail;
-        }
-        else if(profileAddress == null){
-            profileAddress = profileAddress;
-        }
+    public int changeProfileInfo(string profileEmail, string profileAddress){
 
         return 1;
     }
 
-    public int changeProfileName(string profileID, string profilePassword, string profileName){
+    public int changeProfileName(string profileName){
         // Zmena mena na zaklade ID v databaze
         return 1;
     }
 
-    private int generateProfilePrivateSID(int ID){
-        return ID * 35;
-    }
-
-    private int generateProfilePublicSID(int ID){
-        return ID * 70;
-    }
-
-    private string generateHash(string profileName){
+    private string generateHash(string Data){
         SHA256 hash256 = SHA256.Create();
-        byte[] stri = Encoding.ASCII.GetBytes(profileName);
-        byte[] hash = hash256.ComputeHash(stri);
         var sBuilder = new StringBuilder();
 
+        byte[] hash = hash256.ComputeHash(Encoding.ASCII.GetBytes(Data));
+        
         for (int i = 0; i < hash.Length; i++)
         {
             sBuilder.Append(hash[i].ToString("x2"));
         }
 
-        return sBuilder.ToString();;
+        return sBuilder.ToString();
     }
 
 }
